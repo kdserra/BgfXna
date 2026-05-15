bf867970efc671b40456b1fb6662d52c947a3b31
Kevin Serra <kevindanielserra@gmail.com>
2026-05-15 09:46:07 -0400

    sync

--- BgfXna/BGFX/bgfx_dllname.cs
+++ BgfXna/BGFX/bgfx_dllname.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright 2011-2026 Branimir Karadzic. All rights reserved.
  * License: https://github.com/bkaradzic/bgfx/blob/master/LICENSE
  */
@@ -28,9 +28,9 @@ namespace Bgfx
 #elif IOS
         const string DllName = "__Internal";
 #elif DEBUG
-        const string DllName = "bgfx_debug.dll";
+        const string DllName = "bgfx_debug";
 #else
-        const string DllName = "bgfx.dll";
+        const string DllName = "bgfx";
 #endif
     }
 }
